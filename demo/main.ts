334341f26dc89a62f1848544d56c9878542d7b9d
Alexander Kudrin <kudrin.alexander@gmail.com>
2024-10-29 23:20:22 +0300

    Rollback demo/main

--- demo/main.ts
+++ demo/main.ts
@@ -7,32 +7,12 @@ import VanillaCalendar from '@src/vanilla-calendar';
 import '@src/styles/vanilla-calendar.css';
 
 const config: IOptions = {
-	type: 'multiple',
-	months: 12,
-	jump_months: 12,
-	date: {
-		min: '2024-01-01',
-		max: '2025-01-01',
-		today: new Date('2024-01-01T00:00:00.000Z'),
-	},
 	settings: {
-		selection: {
-			day: 'multiple-ranged',
-			month: false,
-			year: false
+		selected: {
+			month: 3,
+			year: 2023,
 		},
-		range: {
-			min: '2024-08-01',
-			max: '2024-10-31',
-			limitMin: 7,
-			limitMax: 14,
-			disabled: ['2024-08-10:2024-08-13', '2024-08-22'],
-		},
-		visibility: {
-			weekend: true,
-			today: false
-		},
-	}
+	},
 };
 
 document.addEventListener('DOMContentLoaded', () => {
